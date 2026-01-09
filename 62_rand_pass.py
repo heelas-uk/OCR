061b72d680e42a6c0b835853172a0698a9ae216a
Alfred Heelas <180876265+heelas-uk@users.noreply.github.com>
2026-01-09 11:13:35 +0000

    Refactor password generation to remove unnecessary output and change output file name

--- 62_rand_pass.py
+++ 62_rand_pass.py
@@ -1,6 +1,5 @@
 import secrets
 num = int(input("Please enter the number of bytes you would like your password to be: "))
 password = str(secrets.token_urlsafe(num))
-print(password)
-with open("rand_pass.secure", "w") as f:
-    f.write("Your password is: \n" + password)
+with open("pass.sir_pilot_in_command", "w") as f:
+    f.write(password)
